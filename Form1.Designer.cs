67bc145bc3b14799fbe8ab08d3b44715fa973fdb
capital.phyoethurakyaw@gmail.com <capital.phyoethurakyaw@gmail.com>
2022-11-26 14:24:25 +0630

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace VFD1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butPolyline = new System.Windows.Forms.Button();
+            this.butCircle = new System.Windows.Forms.Button();
+            this.vdFramedControl1 = new vdControls.vdFramedControl();
+            this.butNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butPolyline
+            // 
+            this.butPolyline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butPolyline.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.butPolyline.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.butPolyline.ForeColor = System.Drawing.Color.White;
+            this.butPolyline.Location = new System.Drawing.Point(1253, 130);
+            this.butPolyline.Name = "butPolyline";
+            this.butPolyline.Size = new System.Drawing.Size(195, 52);
+            this.butPolyline.TabIndex = 30;
+            this.butPolyline.Text = "Add Destination";
+            this.butPolyline.UseVisualStyleBackColor = false;
+            this.butPolyline.Click += new System.EventHandler(this.butPolyline_Click);
+            // 
+            // butCircle
+            // 
+            this.butCircle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butCircle.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.butCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.butCircle.ForeColor = System.Drawing.Color.White;
+            this.butCircle.Location = new System.Drawing.Point(1253, 188);
+            this.butCircle.Name = "butCircle";
+            this.butCircle.Size = new System.Drawing.Size(195, 52);
+            this.butCircle.TabIndex = 24;
+            this.butCircle.Text = "Add Instrument";
+            this.butCircle.UseVisualStyleBackColor = false;
+            this.butCircle.Click += new System.EventHandler(this.butCircle_Click);
+            // 
+            // vdFramedControl1
+            // 
+            this.vdFramedControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.Pane;
+            this.vdFramedControl1.DisplayPolarCoord = false;
+            this.vdFramedControl1.HistoryLines = ((uint)(3u));
+            this.vdFramedControl1.LoadCommandstxt = true;
+            this.vdFramedControl1.LoadMenutxt = true;
+            this.vdFramedControl1.Location = new System.Drawing.Point(12, 11);
+            this.vdFramedControl1.Name = "vdFramedControl1";
+            this.vdFramedControl1.PropertyGridWidth = ((uint)(300u));
+            this.vdFramedControl1.Size = new System.Drawing.Size(1203, 656);
+            this.vdFramedControl1.TabIndex = 43;
+            // 
+            // butNew
+            // 
+            this.butNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butNew.Location = new System.Drawing.Point(1253, 11);
+            this.butNew.Name = "butNew";
+            this.butNew.Size = new System.Drawing.Size(195, 30);
+            this.butNew.TabIndex = 44;
+            this.butNew.Text = "New Document";
+            this.butNew.UseVisualStyleBackColor = true;
+            this.butNew.Click += new System.EventHandler(this.butNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 736);
+            this.Controls.Add(this.butNew);
+            this.Controls.Add(this.vdFramedControl1);
+            this.Controls.Add(this.butPolyline);
+            this.Controls.Add(this.butCircle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button butPolyline;
+        private System.Windows.Forms.Button butCircle;
+        private vdControls.vdFramedControl vdFramedControl1;
+        private System.Windows.Forms.Button butNew;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VFD1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void butCircle_Click(object sender, EventArgs e)
+        {
+
+                butCircle.Enabled = false;
+                //We will create a vdCircle object and add it to the Active Layout which is the basic Model Layout always existing in a Document.
+                VectorDraw.Professional.vdFigures.vdCircle onecircle = new VectorDraw.Professional.vdFigures.vdCircle();
+                //We set the document where the circle is going to be added.This is important for the vdCircle in order to obtain initial properties with setDocumentDefaults.
+                onecircle.SetUnRegisterDocument(vdFramedControl1.BaseControl.ActiveDocument);
+                onecircle.setDocumentDefaults();
+
+                //The two previus steps are important if a vdFigure object is going to be added to a document.
+                //Now we will change some properties of the circle.
+                onecircle.Center = new VectorDraw.Geometry.gPoint(30, 30);
+                onecircle.Radius = 5;
+                onecircle.PenColor.SystemColor = Color.BurlyWood;
+                onecircle.Label = "This is a vdCircle object.";
+                //This line Type is the same indipending from the zoom.Also the LineTypeScale has no effect,See next object for other linetypes.
+                onecircle.LineType = vdFramedControl1.BaseControl.ActiveDocument.LineTypes.DPIDashDotDot;
+
+                //Now we will add this object to the Entities collection of the Model Layout(ActiveLayout).
+                vdFramedControl1.BaseControl.ActiveDocument.ActiveLayOut.Entities.AddItem(onecircle);
+
+                //Zoom in order to see the object.
+                vdFramedControl1.BaseControl.ActiveDocument.ActiveLayOut.ZoomWindow(new VectorDraw.Geometry.gPoint(-100.0, -40.0), new VectorDraw.Geometry.gPoint(140.0, 90.0));
+                //Redraw the document to see the above changes.
+                vdFramedControl1.BaseControl.ActiveDocument.Redraw(true);
+        }
+
+        private void butNew_Click(object sender, EventArgs e)
+        {
+            vdFramedControl1.BaseControl.ActiveDocument.New();
+            vdFramedControl1.BaseControl.ActiveDocument.ShowUCSAxis = false; 
+            butCircle.Enabled = true; 
+            butPolyline.Enabled = true;
+            this.Refresh();
+        } 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vdFramedControl1.SetLayoutStyle(vdControls.vdFramedControl.LayoutStyle.CommandLine, false);
+            vdFramedControl1.SetLayoutStyle(vdControls.vdFramedControl.LayoutStyle.LayoutTab, false);
+            vdFramedControl1.UnLoadMenu();
+            vdFramedControl1.BaseControl.ActiveDocument.ShowUCSAxis = false;
+
+            //We add the "RequiredFiles" folder to the support path. This addition is made in order for our samples to get the required files from this extra folder used in distribution of our sample files.
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\RequiredFiles\\";
+            vdFramedControl1.BaseControl.ActiveDocument.SupportPath = path;
+        }
+
+        private void butPolyline_Click(object sender, EventArgs e)
+        {
+            butPolyline.Enabled = false;
+
+            //We will create a vdPolyline object and add it to the Active Layout which is the basic Model Layout always existing in a Document.
+            VectorDraw.Professional.vdFigures.vdPolyline onepoly = new VectorDraw.Professional.vdFigures.vdPolyline();
+            //We set the document where the polyline is going to be added.This is important for the vdPolyline in order to obtain initial properties with setDocumentDefaults.
+            onepoly.SetUnRegisterDocument(vdFramedControl1.BaseControl.ActiveDocument);
+            onepoly.setDocumentDefaults();
+             
+            onepoly.VertexList.Add(new VectorDraw.Geometry.gPoint(-10, -10));
+            onepoly.VertexList.Add(new VectorDraw.Geometry.gPoint(40, -10)); 
+            onepoly.VertexList.Add(new VectorDraw.Geometry.Vertex(40, 20, 0));
+            onepoly.VertexList.Add(new VectorDraw.Geometry.Vertex(-10, 20, 0));
+         
+            onepoly.Flag = VectorDraw.Professional.Constants.VdConstPlineFlag.PlFlagCLOSE;
+            onepoly.PenColor.ColorIndex = 200;
+
+
+
+            //Now we will add this object to the Entities collection of the Model Layout(ActiveLayout).
+            vdFramedControl1.BaseControl.ActiveDocument.ActiveLayOut.Entities.AddItem(onepoly);
+
+            //Zoom in order to see the object.
+            vdFramedControl1.BaseControl.ActiveDocument.ActiveLayOut.ZoomWindow(new VectorDraw.Geometry.gPoint(-100.0, -40.0), new VectorDraw.Geometry.gPoint(140.0, 90.0));
+            //Redraw the document to see the above changes.
+            vdFramedControl1.BaseControl.ActiveDocument.Redraw(true);
+        }
+    }
+}
